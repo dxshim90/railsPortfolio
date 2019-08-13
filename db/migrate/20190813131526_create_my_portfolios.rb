6a86794b7c5108805887642fe826d67cecc73431
dxshim90 <dxshim90@gmail.com>
2019-08-13 15:10:58 +0100

    Database seeded

--- db/migrate/20190813131526_create_my_portfolios.rb
+++ db/migrate/20190813131526_create_my_portfolios.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateMyPortfolios < ActiveRecord::Migration[5.2]
   def change
     create_table :my_portfolios do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "Blog#{blog}", body: 'LIPSUM LIPSUM LIPSUM LIPSUM LIPSUM LIPSUM LIPSUM LIPSUM LIPSUM '
+  )
+end
+
+puts '10 Blogs created'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Skill #{skill} ", percent: 10
+  )
+end
+
+puts '5 skills created'
+
+9.times do |portfolio_item|
+  MyPortfolio.create!(
+    title: "MyPortfolio_item #{portfolio_item}",
+    subtitle: 'Service',
+    body: 'LIPSUM LIPSUM LIPSUM LIPSUM',
+    main_image: 'https://via.placeholder.com/600x400',
+    tumb_image: 'https://via.placeholder.com/350x150'
+  )
+end
