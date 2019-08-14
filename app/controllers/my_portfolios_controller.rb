6227fa24a23d0012f999e24aeb95101d67883c98
dxshim90 <dxshim90@gmail.com>
2019-08-14 11:28:03 +0100

    Added new portfolio item form

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -5,4 +5,24 @@ class MyPortfoliosController < ApplicationController
     def index 
         @portfolio_items = MyPortfolio.all
     end
+
+def new
+    @portfolio_item = MyPortfolio.new
+end
+
+def create
+    @portfolio_item = MyPortfolio.new(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to my_portfolios_path, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 end
