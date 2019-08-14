140a2854a97876730cfc3a6adfe21aa7f4a8586e
dxshim90 <dxshim90@gmail.com>
2019-08-14 11:58:50 +0100

    added new and edir

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -24,5 +24,24 @@ def create
     end
   end
 
+  def edit
+    @portfolio_item = MyPortfolio.find(params[:id])
+  end
 
-end
+  def update
+    @portfolio_item = MyPortfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to my_portfolios_path, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+      
+  end
+
+
