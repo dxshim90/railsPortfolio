c814e7c26890a8e9df8743c6239832603befdc54
dxshim90 <dxshim90@gmail.com>
2019-08-16 10:54:28 +0100

    validation added to modals

--- app/models/my_portfolio.rb
+++ app/models/my_portfolio.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class MyPortfolio < ApplicationRecord
+    validates_presence_of :title, :body, :main_image, :tumb_image
+    
+    
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Skill < ApplicationRecord
+    validates_presense_of :title, :percent
 end
